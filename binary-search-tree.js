284213384be88e30dc71c61976ca763a5ff7f547
Kris Mendoza <thekittengallery@gmail.com>
2025-02-26 02:14:20 +0800

    Implement insert() and update #buildTree()
    - Add insert() for correctly inserting new values to the tree
    - Fix #buildTree() to correctly handle empty arrays

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -10,7 +10,6 @@ class Tree {
   constructor(values) {
     this.values = values;
     this.root = this.#buildTree(this.#sort(this.values));
-    this.prettyPrint(this.root);
   }
 
   #sort(array) {
@@ -18,6 +17,7 @@ class Tree {
   }
 
   #buildTree(array) {
+    if (!array || array.length === 0) return null;
     if (array.length <= 1) return new Node(array[0]);
 
     const mid = Math.floor(array.length / 2);
@@ -41,6 +41,21 @@ class Tree {
     console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
     if (node.left !== null)
       this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+  }
+
+  insert(value, node = this.root) {
+    if (!this.root) this.root = new Node(value);
+
+    if (!node) return new Node(value);
+    if (node.data === value) return node;
+
+    if (node.data > value) {
+      node.left = this.insert(value, node.left);
+    } else {
+      node.right = this.insert(value, node.right);
+    }
+
+    return node;
   }
 }
 
--- main.js
+++ main.js
@@ -3,3 +3,11 @@ import Tree from './binary-search-tree.js';
 const data = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 
 const tree = new Tree(data);
+
+tree.insert(29);
+tree.insert(99);
+tree.insert(993);
+tree.insert(-50);
+tree.insert(7);
+
+tree.prettyPrint(tree.root);
